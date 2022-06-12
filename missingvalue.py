78d81c185730b45063db67f94aa23ef4c1dc1a42
LakshayHasija <lakshay.hasija2002@gmail.com>
2022-06-12 14:45:57 +0530

    Update missingvalue.py

--- missingvalue.py
+++ missingvalue.py
@@ -9,6 +9,38 @@ train = pd.read_csv('titanic_train.csv')
 # sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
 # plt.show()
 
+# sns.set_style('whitegrid')
+# sns.countplot(x='Survived', hue='Pclass', data=train)
+# plt.show()
+
+# sns.histplot(train['Age'].dropna(), kde=False, color='darkred', bins=40)
+# plt.show()
+
 sns.set_style('whitegrid')
-sns.countplot(x='Survived', hue='Pclass', data=train)
+sns.boxplot(x='Pclass', y='Age', data=train)
+# plt.show()
+
+
+def impute_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+
+    if pd.isnull(Age):
+
+        if Pclass == 1:
+            return 37
+
+        elif Pclass == 2:
+            return 29
+
+        else:
+            return 24
+
+    else:
+        return Age
+
+
+train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis=1)
+train.drop('Cabin', axis=1, inplace=True)
+sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
 plt.show()
